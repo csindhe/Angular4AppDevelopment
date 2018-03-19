2ca221016dc9f5b0b4cc978af45d530b68c616b7
Chethan Srinivas Rao <csindhe@gmail.com>
2018-03-19 19:21:43 +0530

    Fourteenth Commit

--- chat-demo/src/app/app.module.ts
+++ chat-demo/src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { ChatNavBarComponent } from './chatnavbar/chatnavbar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatNavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ chat-demo/src/app/chatnavbar/chatnavbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'app-chatnavbar',
+    templateUrl: './chatnavbar.component.html',
+    styleUrls: ['./chatnavbar.component.css']
+})
+export class ChatNavBarComponent implements OnInit {
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+        
+    }
+}
