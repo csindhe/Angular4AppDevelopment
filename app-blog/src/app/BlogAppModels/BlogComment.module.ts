3538fdd19a89c6b316f62474eea97db19dd96d76
Chethan Srinivas Rao <csindhe@gmail.com>
2018-04-10 10:55:50 +0530

    nth commit

---
+++ app-blog/src/app/BlogAppModels/BlogComment.module.ts
@@ -0,0 +1,12 @@
+export class BlogComment {
+    id: any;
+    parentId: any;
+    blogId: any;
+    content: string;
+    time: Date;
+    author: string;
+
+    constructor() {
+        
+    }
+}
